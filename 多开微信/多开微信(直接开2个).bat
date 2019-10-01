7fad127bbdfbf17b7fcecad953bbca27d8b3c27d
Dichters <dichters@163.com>
2019-10-02 00:14:25 +0100

    加了点注释

--- 多开微信/多开微信(直接开2个).bat
+++ 多开微信/多开微信(直接开2个).bat
@@ -1,4 +1,9 @@
 @echo off
+:: 原理：瞬间同时打开多个微信，避免微信检测到当前是否已开微信（效果相当于狂点微信快捷方式多次）
+:: 请自行修改微信所在路径
+:: 请按需修改重复打开的个数，一行开一个
+:: 出处已不可考
+
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
--- 开放卓越性能/win10开启卓越性能（1709以上可用）.bat
+++ 开放卓越性能/win10开启卓越性能（1709以上可用）.bat
@@ -1,5 +1,5 @@
 :: win10 1709以上可以新增卓越性能
-:: 执行此bat前请先执行前置reg以解锁自定义电源方案
+:: 执行此bat前，请先执行前置reg以解锁自定义电源方案
 
 @echo off
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
