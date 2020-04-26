557e5ee1b9cb562a59893ff64e79e74407fcee16
zergneptune <dichters@163.com>
2020-04-26 22:35:16 +0800

    paushe

--- 自动添加JAVA,MAVEN等环境变量/添加环境变量.bat
+++ 自动添加JAVA,MAVEN等环境变量/添加环境变量.bat
@@ -4,3 +4,4 @@ set JAVA_HOME=E:\dev\ENV\jdk-13.0.2
 set PATH=%PATH%;%JAVA_HOME%\bin;%MAVEN_HOME%\bin
 ::set CLASSPATH=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar; 
 set MAVEN_HOME=E:\dev\ENV\apache-maven-3.6.3
+pause
